88c76c9b344c8d7ef5787b775f4144b80f7518da
HuyDucVo <huydvo95@gmail.com>
2018-09-11 22:57:17 -0700

    Initial Commit

---
+++ Tip Calculator-Prework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator-Prework
+//
+//  Created by Harry Vo on 9/11/18.
+//  Copyright © 2018 Harry Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
